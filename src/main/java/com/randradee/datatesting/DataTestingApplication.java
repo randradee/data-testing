94e19fa6ebf057e90111b99d6f24dd82ad122b72
Renato Andrade <dev.renatopandrade@gmail.com>
2023-04-18 18:04:46 -0300

    initial commit

---
+++ src/main/java/com/randradee/datatesting/DataTestingApplication.java
@@ -0,0 +1,13 @@
+package com.randradee.datatesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataTestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataTestingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/randradee/datatesting/DataTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.randradee.datatesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataTestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
